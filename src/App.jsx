e74d0b72205bff2d7cd54dfedb17f387ce5daf6b
Ashimolowo <royins64@gmail.com>
2024-08-12 07:18:19 -0700

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <>
-      <main className='container' style={{paddingTop: "100px"}}>
+      <main className='container' style={{paddingTop: "100px", fontSize: "18px", fontWeight: "600"}}>
       <Movies/>
       </main>
       
